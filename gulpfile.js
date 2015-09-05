559e1f4fad53749fba04259741f388d9565a7b39
Amjith Ramanujam <amjith.r@gmail.com>
2015-09-04 22:35:02 -0700

    Initial commit.

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+'use strict'
+
+var gulp = require('gulp')
+var elm = require('gulp-elm')
+
+gulp.task('elm-init', elm.init)
+
+gulp.task('elm', ['elm-init'], function () {
+  return gulp.src('Bingo.elm')
+    .pipe(elm())
+    .pipe(gulp.dest('./'))
+})
