d2f5de841793d46cba06e0a5ba2b487b85e8514f
Amjith Ramanujam <amjith.r@gmail.com>
2015-09-06 22:08:32 -0700

    Separate view and modela and use startapp.

--- Bingo.elm
+++ Bingo.elm
@@ -5,6 +5,41 @@ import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 import String exposing (toUpper, repeat, trimRight)
 
+import StartApp
+
+-- Model
+
+newEntry phrase points id = 
+  { phrase = phrase,
+    points = points,
+    wasSpoken = False,
+    id = id
+  }
+
+initialModel = 
+  { entries = 
+      [ newEntry "Doing Agile" 100 2,
+        newEntry "Future Proof" 300 3,
+        newEntry "Touch Base" 400 1,
+        newEntry "In the cloud" 200 4
+      ] 
+  } 
+
+-- Update
+
+type Action 
+  = NoOp 
+  | Sort
+
+update action model = 
+  case action of
+    NoOp -> 
+      model
+    Sort ->
+      { model | entries <- List.sortBy .points model.entries }
+
+-- View
+
 title message times = 
   message ++ " "
     |> toUpper 
@@ -20,28 +55,34 @@ pageHeader =
 pageFooter = 
   footer [  ] 
     [ a [ href "http://blog.amjith.com" ] 
-        [ text "Amjith" ] 
+        [ text "Amjith Ramanujam" ] 
     ] 
 
-entryItem phrase points = 
+entryItem entry =
   li [  ]
-    [ span [ class "phrase" ] [ text phrase ],
-      span [ class "points" ] [ text (toString points) ] 
+    [ span [ class "phrase" ] [ text entry.phrase ],
+      span [ class "points" ] [ text (toString entry.points) ] 
     ]
 
-entryList = 
-  ul [  ]
-      [ entryItem "Future-Proof" 100,
-        entryItem "Doing Agile" 200
-      ]
+entryList entries = 
+  ul [  ] (List.map entryItem entries) 
 
-view = 
+view address model = 
   div [ id "container" ] 
     [ pageHeader, 
-      entryList,
+      entryList model.entries,
+      button 
+        [ class "sort", onClick address Sort ] 
+        [ text "sort" ],
       pageFooter 
     ]
 
+-- Wire it all together
 
 main = 
-  view
+  -- view (update Sort initialModel)
+  StartApp.start
+    { model = initialModel,
+      view = view,
+      update = update
+    }
